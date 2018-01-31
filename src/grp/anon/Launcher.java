a7154efdba23e3bb4268b0ba7760590a6d67d0e6
Anikesh <anikeshpatel4@gmail.com>
2018-01-31 14:12:03 +0530

    Animation Bug Fixed

--- src/grp/anon/Launcher.java
+++ src/grp/anon/Launcher.java
@@ -4,16 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Launcher extends Application {
+    private static Stage mainStage;
     @Override
     public void start(Stage primaryStage) throws Exception {
+        mainStage = primaryStage;
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("recruiter/Recruiter_Window.fxml")));
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static Stage getWindow(){
+        return mainStage;
+    }
+    public static void minimize(){
+        mainStage.setIconified(true);
     }
 }
--- src/grp/anon/recruiter/RecruiterWindow.java
+++ src/grp/anon/recruiter/RecruiterWindow.java
@@ -1,13 +1,19 @@
 package grp.anon.recruiter;
 
 
+import grp.anon.Launcher;
 import javafx.animation.TranslateTransition;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Window;
 import javafx.util.Duration;
 
 import java.net.URL;
@@ -15,8 +21,20 @@ import java.util.ResourceBundle;
 
 public class RecruiterWindow implements Initializable {
 
+
+    @FXML
+    private BorderPane root;
     @FXML
     private AnchorPane drawer_pane;
+
+    @FXML
+    private HBox windowDock;
+
+    @FXML
+    private ImageView minBtn;
+
+    @FXML
+    private ImageView exitBtn;
 
     @FXML
     private ImageView hamburger;
@@ -36,20 +54,53 @@ public class RecruiterWindow implements Initializable {
         engine = profilePic.getEngine();
         engine.load(getClass().getResource("../inssets/profilePic.html").toExternalForm());
         drawerOpenTransition = new TranslateTransition(Duration.seconds(1),drawer);
-        drawerOpenTransition.setFromX(0);
         drawerOpenTransition.setToX(300);
         drawerCloseTransition = new TranslateTransition(Duration.seconds(0.5),drawer);
-        drawerCloseTransition.setFromX(300);
         drawerCloseTransition.setToX(0);
         hamburger.setOnMouseClicked(e->{
             if (toggle == -1){
+                drawerOpenTransition.setFromX(drawer.getTranslateX());
                 drawerOpenTransition.play();
                 toggle *= -1;
             }
             else{
+                drawerCloseTransition.setFromX(drawer.getTranslateX());
                 drawerCloseTransition.play();
                 toggle *= -1;
             }
         });
+
+        exitBtn.setOnMouseClicked(e->{
+            exit();
+        });
+        minBtn.setOnMouseClicked(e->{
+            minimize();
+        });
+
+        final double[] xOffset = new double[1];
+        final double[] yOffset = new double[1];
+        windowDock.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset[0] = event.getSceneX();
+                yOffset[0] = event.getSceneY();
+            }
+        });
+
+        windowDock.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                root.getScene().getWindow().setX(event.getScreenX() - xOffset[0]);
+                root.getScene().getWindow().setY(event.getScreenY() - yOffset[0]);
+            }
+        });
+    }
+
+    private void exit(){
+        System.exit(0);
+    }
+
+    private void minimize(){
+        Launcher.minimize();
     }
 }
